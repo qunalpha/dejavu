47c52f641f13fff95c77d48a4243b367dcc31347
Quna <iquna2011@gmail.com>
2026-09-13 02:12:36 +0530

    App Lister
    usage: call getWindowList() to get tuple list of application running in your desktop

---
+++ appList.py
@@ -0,0 +1,22 @@
+import win32gui
+import win32process
+import psutil
+
+def getWindowList():
+    application_list = []
+
+    def enum_window_callback(hwnd, extra):
+        if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd).strip():
+            _, pid = win32process.GetWindowThreadProcessId(hwnd)
+            exe = psutil.Process(pid).name()
+            name = win32gui.GetWindowText(hwnd)
+
+            application_list.append((pid, exe, name))
+
+    win32gui.EnumWindows(enum_window_callback, None)
+
+    return application_list
+
+if __name__ == "__main__":
+    for i in getWindowList():
+        print(f"{i[0]:<10} | {i[1]:<30} | {i[2]}")
